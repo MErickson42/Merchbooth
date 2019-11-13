eb95f499753e6b030c53e85a182264d1eb976729
Matt Erickson <merickson@magnetdigitalanddata.com>
2019-11-13 08:51:30 -0500

    MDE - Nov. 13th. Added Products.aspx and ModifyProducts.aspx in the admin folder. Also added Uploads folder for uploading product images. We are now able to add and modify new products - but only for 1 band until we get login working

---
+++ Merchbooth/Admin/Products.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="Products" Language="C#" MasterPageFile="~/Admin/AdminMaster.Master" AutoEventWireup="true" CodeBehind="Products.aspx.cs" Inherits="Merchbooth.Admin.Products" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="row">
+        <div class="col-lg-1 col-sm-1"></div>
+        <div class="col-lg-10 col-sm-10">
+            <ul class="breadcrumb pull-right">
+                <li><a href="/Admin/">Home</a></li>
+                <li class="active">Content</li>
+            </ul>
+        </div>
+        <div class="col-lg-1 col-sm-1"></div>
+    </div>
+    <div class="row">
+        <div class="col-lg-1 col-sm-1"></div>
+        <div class="col-lg-10 col-sm-10">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+                    <h1>Content</h1>
+                </div>
+            </div>
+            <div class="row"><div class="col-lg-12 col-sm-12" style="text-align: center;"><asp:Label ID="lblMessage" runat="server" CssClass="errormessage" Text=""></asp:Label></div></div>
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+                    <asp:Label ID="lblProducts" runat="server" Text=""></asp:Label>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-1 col-sm-1"></div>
+    </div>
+
+    <script type="text/javascript">
+        var deleteLinks = document.querySelectorAll('.delete');
+
+        for (var i = 0; i < deleteLinks.length; i++) {
+            deleteLinks[i].addEventListener('click', function (event) {
+                event.preventDefault();
+
+                var choice = confirm(this.getAttribute('data-confirm'));
+
+                if (choice) {
+                    window.location.href = this.getAttribute('href');
+                }
+            });
+        }
+    </script>
+</asp:Content>
---
+++ Merchbooth/Admin/Products.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Merchbooth.Admin
+{
+    public partial class Products : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = Server.UrlDecode(Request.QueryString["message"]);
+
+            SiteDCDataContext _siteContent = new SiteDCDataContext();
+
+            var queryProducts = from p in _siteContent.TProducts
+
+                                orderby p.intProductID
+                                // MDE - For now - we will just query band 1 until we have a functioning login system
+                                where p.intBandID == 1
+                                select p;
+
+            queryProducts.ToList();
+
+            StringBuilder tl = new StringBuilder();
+
+            tl.Append("<table class='table table-condensed'>");
+            tl.Append("<thead>");
+            tl.Append("<tr><th colspan='3' class='alignright'><strong><a href='/Admin/ModifyProduct.aspx'>add Content &nbsp; <i class='fa fa-plus fa-lg'></i></a></strong></th></tr>");
+            tl.Append("</thead>");
+            tl.Append("<tbody>");
+
+            if (queryProducts.Count() > 0)
+            {
+                foreach (var item in queryProducts)
+                {
+                   
+
+                    tl.Append("<tr><td><strong>" + item.strProductName + "</strong></td><td><strong>" + "</strong></td><td style='width: 300px;'><div class='pull-right'><strong><a href='/Admin/ModifyProduct.aspx?pk=" + item.intProductID + "'>Edit Product <i class='fa fa-pencil-square-o fa-lg'></i></a> &nbsp; | &nbsp; <a href ='/Admin/ModifyProduct.aspx?pk=" + item.intProductID + "&a=d' class='delete' data-confirm='Are you sure to delete this product?'>Delete Product <i class='fa fa-trash fa-lg'></i></a></strong></div></td></tr>");
+
+                }
+                tl.Append("<tr><td colspan='3'>&nbsp;</td></tr>");
+            }
+            else
+            {
+                tl.Append("<tr><td colspan='3'>No Content currently available.</td></tr>");
+            }
+
+            tl.Append("</tbody>");
+            tl.Append("</table>");
+
+            lblProducts.Text = tl.ToString();
+        }
+    }
+}
